787ba617a7475e66ce2d1d04c3c3d3f83df92bd4
joseiturriaga25amz <jose.iturriaga25@gmail.com>
2026-09-19 17:31:40 -0300

    full: allow repeat ultimate starter tutor lessons

--- src/field_specials.c
+++ src/field_specials.c
@@ -2242,22 +2242,19 @@ bool8 CapeBrinkGetMoveToTeachLeadPokemon(void)
     {
         StringCopy(gStringVar2, gMoveNames[MOVE_FRENZY_PLANT]);
         gSpecialVar_0x8005 = MOVETUTOR_FRENZY_PLANT;
-        if (FlagGet(FLAG_TUTOR_FRENZY_PLANT) == TRUE)
-            return FALSE;
+        gSpecialVar_0x8008 = FlagGet(FLAG_TUTOR_FRENZY_PLANT);
     }
     else if (tutorMonId == 1)
     {
         StringCopy(gStringVar2, gMoveNames[MOVE_BLAST_BURN]);
         gSpecialVar_0x8005 = MOVETUTOR_BLAST_BURN;
-        if (FlagGet(FLAG_TUTOR_BLAST_BURN) == TRUE)
-            return FALSE;
+        gSpecialVar_0x8008 = FlagGet(FLAG_TUTOR_BLAST_BURN);
     }
     else
     {
         StringCopy(gStringVar2, gMoveNames[MOVE_HYDRO_CANNON]);
         gSpecialVar_0x8005 = MOVETUTOR_HYDRO_CANNON;
-        if (FlagGet(FLAG_TUTOR_HYDRO_CANNON) == TRUE)
-            return FALSE;
+        gSpecialVar_0x8008 = FlagGet(FLAG_TUTOR_HYDRO_CANNON);
     }
     if (GetMonData(&gPlayerParty[leadMonSlot], MON_DATA_MOVE1) != MOVE_NONE)
         numMovesKnown++;
