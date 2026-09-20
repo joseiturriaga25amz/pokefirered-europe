712e86b36692f695c0107d3c9cb09f6de33def1f
joseiturriaga25amz <jose.iturriaga25@gmail.com>
2026-09-19 22:52:57 -0300

    full: keep roamers active after KO and Roar

--- src/battle_main.c
+++ src/battle_main.c
@@ -4006,10 +4006,10 @@ static void ReturnFromBattleToOverworld(void)
         {
             UpdateRoamerHPStatus(&gEnemyParty[0]);
 #if defined(BUGFIX) || REVISION >= 0xA
-            if ((gBattleOutcome == B_OUTCOME_WON) || gBattleOutcome == B_OUTCOME_CAUGHT || gBattleOutcome == B_OUTCOME_DREW)
+            if (gBattleOutcome == B_OUTCOME_CAUGHT)
 #else
-            if ((gBattleOutcome & B_OUTCOME_WON) || gBattleOutcome == B_OUTCOME_CAUGHT) // Bug: When Roar is used by roamer, gBattleOutcome is B_OUTCOME_PLAYER_TELEPORTED (5).
-#endif                                                                                  // & with B_OUTCOME_WON (1) will return TRUE and deactivates the roamer.
+            if (gBattleOutcome == B_OUTCOME_CAUGHT)
+#endif
                 SetRoamerInactive();
         }
         m4aSongNumStop(SE_LOW_HEALTH);
