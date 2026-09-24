42a6cbd3dfb7f10072f33a6e1c59c920e2bc695e
joseiturriaga25amz <jose.iturriaga25@gmail.com>
2026-09-24 13:07:07 -0300

    B1.1: relock intentionally changed script menu blob

--- tools/validate_audited_data_blobs.py
+++ tools/validate_audited_data_blobs.py
@@ -21,7 +21,7 @@ EXPECTED = {
     "data/scripts/spanish/hall_of_fame.inc": "9e2c9bb7e8b0bb3537aa313bae9ffcf405f1630c",
     "data/scripts/spanish/repel.inc": "d0255413c6a5453d5e1af15201b41d8b4663f22b",
     "data/scripts/spanish/move_tutors.inc": "f6fc0e15f3c7225bef69ae4d6362fced3c0e60f1",
-    "src/script_menu.c": "52174b0d3f5d1f659a52f6674b7e920f8a78a657",
+    "src/script_menu.c": "d0939bf7f3c0c8b6409bad5f6b1508cdb3814836",
     "data/maps/SixIsland_AlteringCave/scripts.inc": "da91f6e0f72a0a360b53abdc839dc6f11a0b903a",
     "data/maps/SixIsland_AlteringCave/map.json": "0e2358007cc22f8d31bc93226e2801b9a8f06d81",
     "src/wild_encounter.c": "293caa0f44694ce9aefc734cde02256467e2fa9f",
