64a63687c20a1f4926b834a9aa0c2edd3a5caba2
joseiturriaga25amz <jose.iturriaga25@gmail.com>
2026-09-25 09:07:08 -0300

    B1 HM: allow replacing learned HM moves

--- src/pokemon_summary_screen.c
+++ src/pokemon_summary_screen.c
@@ -3889,13 +3889,6 @@ static void UpdateCurrentMonBufferFromPartyOrBox(struct Pokemon * mon)
 
 static u8 PokeSum_CanForgetSelectedMove(void)
 {
-    u16 move;
-
-    move = GetMonMoveBySlotId(&sMonSummaryScreen->currentMon, sMoveSelectionCursorPos);
-
-    if (IsMoveHm(move) == TRUE && sMonSummaryScreen->mode != PSS_MODE_FORGET_MOVE)
-        return FALSE;
-
     return TRUE;
 }
 
